000946c192c28b5b33b6532ea01d29bdbe5dc1e4
Esteban <esteban@fwd.dev>
2022-06-28 18:38:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-curl -s -L "https://github.com/fwd/n1/raw/dev/n1.sh" -o /usr/local/bin/n2
+curl -s -L "https://github.com/fwd/n1/raw/dev/n1.sh" -o /usr/local/bin/n1
 sudo chmod +x /usr/local/bin/n1
 echo "N1 is now installed."
